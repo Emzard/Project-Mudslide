670a6ec22c9dbd95123fc724c6256355a740e02a
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2024-11-11 10:35:08 +0300

    Player Controller
    -add player controller script

--- MudSlide/Assets/Scripts/PlayerController.cs
+++ MudSlide/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float jumpForce = 5f;
+    public float moveSpeed = 5f;
+
+    private new Rigidbody rigidbody;
+    private bool isGrounded; 
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(horizontalInput *moveSpeed, rigidbody.velocity.y, 0);
+        rigidbody.velocity = new Vector3(movement.x, rigidbody.velocity.y, rigidbody.velocity.z);
         
+        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 }
