6e1024b3c8384f488615e6623822b25de1af9ef9
Emzard <generalemzard@gmail.com>
2024-11-19 19:32:31 +0100

    Object spawning, movement, sound
    Obstacles, collectibles and powerups now spawn. Made some adjustments to the player's movement and added sounds to the game.

---
+++ MudSlide/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    private float speed = 25;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
+
+        if (transform.position.z < -5)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- MudSlide/Assets/Scripts/PlayerController.cs
+++ MudSlide/Assets/Scripts/PlayerController.cs
@@ -4,57 +4,111 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public float jumpForce = 15f;
-    public float moveSpeed = 15f;
+    private float jumpForce = 5.0f;
+    private float moveSpeed = 10.0f;
+    public int collectibles = 0;
 
+    public AudioClip collectibleSound;
+    public AudioClip collisionSound;
+    public AudioClip jumpSound;
+    public AudioClip powerupSound;
+    public AudioClip gameOverSound;
+
+    private AudioSource playerAudio;
+    private AudioSource mainCameraAudio;
     private new Rigidbody rigidbody;
-    private bool isGrounded; 
+    private bool isGrounded = true;
+
+    // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
+        playerAudio = GetComponent<AudioSource>();
+        mainCameraAudio = GameObject.Find("Main Camera").GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
-
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            Jump();
-        }
-
+        Jump();
     }
 
     private void Move()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 move = new Vector3(horizontal, 0, vertical).normalized*moveSpeed*Time.deltaTime;
-
+        /* Vector3 move = new Vector3(horizontal, 0, 0).normalized * moveSpeed * Time.deltaTime;
         rigidbody.MovePosition(transform.position + transform.TransformDirection(move));
+        */
+
+        transform.Translate(Vector3.right * horizontal * moveSpeed * Time.deltaTime);
     }
 
     private void Jump()
     {
-        rigidbody.AddForce(Vector3.up*jumpForce, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+
+            playerAudio.PlayOneShot(jumpSound, 0.2f);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        // This allows the player to jump again
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
         }
+
+        // Prints game over when the player hits an obstacle
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            playerAudio.PlayOneShot(collisionSound, 0.2f);
+            playerAudio.PlayOneShot(gameOverSound, 0.2f);
+            mainCameraAudio.Stop();
+
+            Debug.Log("Game Over!");
+        }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if(collision.gameObject.CompareTag("Ground"))
+        // Collectibles are destroyed and then added to the player
+        if (other.CompareTag("Collectible"))
+        {
+            collectibles++;
+            Destroy(other.gameObject);
+            Debug.Log("You have " + collectibles + " collectibles.");
+
+            playerAudio.PlayOneShot(collectibleSound, 0.2f);
+        }
+
+        if (other.CompareTag("Speed Boost"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("You got a speed boost!");
+
+            playerAudio.PlayOneShot(powerupSound, 0.7f);
+        }
+
+        if (other.CompareTag("Shield"))
+        {
+            Destroy(other.gameObject);
+            Debug.Log("You got a shield!");
+
+            playerAudio.PlayOneShot(powerupSound, 0.7f);
+        }
+    }
+
+    /* private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
         }
-    }
+    } */
 }
