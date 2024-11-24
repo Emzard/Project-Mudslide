14ca388ca7f0691b6e618929fd4805101a5dfd85
Kretoskim <98364505+kretoskim@users.noreply.github.com>
2024-11-24 12:44:17 +0300

    -Crocodile animations
    -Bite animation

--- MudSlide/Assets/Scripts/Crocodile.cs
+++ MudSlide/Assets/Scripts/Crocodile.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Crocodile : MonoBehaviour
 {
     public SkinnedMeshRenderer skinnedMeshRenderer;
+    public Transform player;
     public int Swimming = 0;
     public int Bite = 1;
 
     public float amplitude = 50f;
-    public float frequency = 1f;
+    public float frequency = 3f;
+    public float biteDistance = 15f;
+    private bool isBiting = false;
 
     // Update is called once per frame
     void Update()
@@ -18,6 +21,29 @@ public class Crocodile : MonoBehaviour
        {
         float weight = amplitude * (Mathf.Sin(Time.time*frequency*2*Mathf.PI)*0.5f+0.5f);
         skinnedMeshRenderer.SetBlendShapeWeight(Swimming, weight);
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        if(distanceToPlayer <= biteDistance && !isBiting)
+        {
+            TriggerBite();
+        }
+        else if(distanceToPlayer > biteDistance && isBiting)
+        {
+            StopBite();
+        }
        } 
+    }
+
+    private void TriggerBite()
+    {
+        isBiting = true;
+        skinnedMeshRenderer.SetBlendShapeWeight(Bite, 100f);
+    }
+
+    private void StopBite()
+    {
+        isBiting = false;
+        skinnedMeshRenderer.SetBlendShapeWeight(Bite, 0f);
     }
 }
