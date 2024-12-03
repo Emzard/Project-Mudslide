5dad9c34cf148a299edd9101a7609f81679e89e5
Apilli Barbra <bibairene.apilli01@hotmail.com>
2024-12-03 09:44:54 +0300

    removed the fourth heart

--- MudSlide/Assets/Scripts/HealthManager.cs
+++ MudSlide/Assets/Scripts/HealthManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class HealthManager : MonoBehaviour
 {
-    public GameObject heart0, heart1, heart2, heart3, heart4;
+    public GameObject heart0, heart1, heart2, heart3; // heart4;
     public GameObject GameOverScreen;
     public static int health;
     // Start is called before the first frame update
@@ -24,7 +24,7 @@ public class HealthManager : MonoBehaviour
                 heart1.SetActive(false);
                 heart2.SetActive(false);
                 heart3.SetActive(false);
-                heart4.SetActive(false);
+                //heart4.SetActive(false);
                 Time.timeScale = 0;
                 GameOverScreen.SetActive(true);
                 break;
@@ -33,35 +33,35 @@ public class HealthManager : MonoBehaviour
                 heart1.SetActive(true);
                 heart2.SetActive(false);
                 heart3.SetActive(false);
-                heart4.SetActive(false);
+                //heart4.SetActive(false);
                 break;
             case 2:
                 //heart0.SetActive(true);
                 heart1.SetActive(true);
                 heart2.SetActive(true);
                 heart3.SetActive(false);
-                heart4.SetActive(false);
+                //heart4.SetActive(false);
                 break;
             case 3:
                 //heart0.SetActive(true);
                 heart1.SetActive(true);
                 heart2.SetActive(true);
                 heart3.SetActive(true);
-                heart4.SetActive(false);
+                //heart4.SetActive(false);
                 break;
             case 4:
                 //heart0.SetActive(true);
                 heart1.SetActive(true);
                 heart2.SetActive(true);
                 heart3.SetActive(true);
-                heart4.SetActive(true);
+                //heart4.SetActive(true);
                 break;
             default: // Game Over
                 //heart0.SetActive(false);
                 heart1.SetActive(false);
                 heart2.SetActive(false);
                 heart3.SetActive(false);
-                heart4.SetActive(false);
+                //heart4.SetActive(false);
                 Time.timeScale = 0;
                 GameOverScreen.SetActive(true);
                 break;
